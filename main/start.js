9500e7680df91537454373d904a7db65a93b05d3
Poy <108749548+PoyOzk@users.noreply.github.com>
2023-09-26 19:56:13 -0400

    Update start.js

--- main/start.js
+++ main/start.js
@@ -1,5 +1,3 @@
-
-
 document.querySelector("html body div.container-fluid div.flexbar").insertAdjacentHTML("afterbegin", `<button type="button" data-bs-toggle="modal" data-bs-target="#rhsModal" style="background: #ffffff1c;top: -4px;position: relative;color: white;" class="btn menu-rhs">
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
 <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"></path>
